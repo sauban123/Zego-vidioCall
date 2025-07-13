453ac46da82596369c3c5e3c57914c0d35954615
mohd sauban <mosauban88@gmail.com>
2025-07-14 00:00:00 +0530

    finish

---
+++ app/src/main/java/com/example/vidiocallapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.vidiocallapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.zegocloud.uikit.prebuilt.call.ZegoUIKitPrebuiltCallConfig
+import com.zegocloud.uikit.prebuilt.call.ZegoUIKitPrebuiltCallFragment
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val APP_ID : Long = 1102039514
+        const val APP_SIGN = "da2e3c456a3d723e90ca4e7aea6765c553dc40db5392bbafeb7a3f35ea5d8dbb"
+        const val CALL_ID = "12345"
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.submitButton)
+        val username = findViewById<EditText>(R.id.userId)
+        val userid = findViewById<EditText>(R.id.Password)
+        button.setOnClickListener {
+            val confic=  ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
+            val fragment = ZegoUIKitPrebuiltCallFragment.newInstance(
+                APP_ID, APP_SIGN,userid.text.toString(),username.text.toString(), CALL_ID, confic
+            )
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainLayout, fragment)
+                .commitNow()
+        }
+    }
+}
